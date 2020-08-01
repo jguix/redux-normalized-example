6dd12fbe504cfe41c3936141f6bc047c202a9536
Juan G. Jordán <juangui@gmail.com>
2020-08-02 01:34:24 +0200

    Added concurrently server launch and server proxy

--- src/modules/comment/comment.api.ts
+++ src/modules/comment/comment.api.ts
@@ -1,11 +1,11 @@
 import { Comment } from './comment.types';
 
 const loadComments = (): Promise<Comment[]> => {
-  return fetch(`http://localhost:3001/comments`).then((response) => response.json());
+  return fetch(`/comments`).then((response) => response.json());
 };
 
 const loadPostComments = (postId: number): Promise<Comment[]> => {
-  return fetch(`http://localhost:3001/comments?postId=${postId}`).then((response) => response.json());
+  return fetch(`/comments?postId=${postId}`).then((response) => response.json());
 };
 
 export const commentApi = { loadComments, loadPostComments };
--- src/modules/post/post.api.ts
+++ src/modules/post/post.api.ts
@@ -1,11 +1,11 @@
 import { Post } from './post.types';
 
 const loadPosts = (): Promise<Post[]> => {
-  return fetch(`http://localhost:3001/posts`).then((response) => response.json());
+  return fetch(`/posts`).then((response) => response.json());
 };
 
 const loadUserPosts = (userId: number): Promise<Post[]> => {
-  return fetch(`http://localhost:3001/posts?userId=${userId}`).then((response) => response.json());
+  return fetch(`/posts?userId=${userId}`).then((response) => response.json());
 };
 
 export const postApi = { loadPosts, loadUserPosts };
